9ad4acbf973f47d934718347f3a33305ef0647ba
ToniMBIX <antmarbou@alu.edu.gva.es>
2023-05-16 01:31:59 +0200

    Agregar archivos de proyecto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace CouplesGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 4;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Font = new System.Drawing.Font("Webdings", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(2)));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(532, 503);
+            this.tableLayoutPanel1.TabIndex = 0;
+            this.tableLayoutPanel1.Tag = "Cc";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(532, 503);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "CouplesGame";
+            this.ResumeLayout(false);
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CouplesGame
+{
+    public partial class Form1 : Form
+    {
+        private Random random = new Random();
+        private List<char> characters = new List<char>();
+        private Button firstSelectedButton = null;
+        private Button secondSelectedButton = null;
+        private Timer checkForMatchTimer;
+        private Timer gameTimer;
+        private Label timeLabel;
+        private int timeElapsed;
+        private Timer hideFirstSelectionTimer;
+
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCharacters();
+            InitializeTimer();
+            hideFirstSelectionTimer = new Timer
+            {
+                Interval = 3000,
+                Enabled = false
+            };
+            hideFirstSelectionTimer.Tick += HideFirstSelectionTimer_Tick;
+            timeElapsed = 0;
+            timeLabel = new Label
+            {
+                Text = "Segundos: 0",
+                Dock = DockStyle.Top
+            };
+            this.Controls.Add(timeLabel);
+
+            gameTimer = new Timer
+            {
+                Interval = 1000,
+                Enabled = true
+            };
+            gameTimer.Tick += GameTimer_Tick;
+            for (int i = 0; i < tableLayoutPanel1.RowCount; i++)
+            {
+                for (int j = 0; j < tableLayoutPanel1.ColumnCount; j++)
+                {
+                    Button button = new Button
+                    {
+                        Dock = DockStyle.Fill,
+                        Font = new Font("Webdings", 48F, FontStyle.Bold),
+                        Tag = GetRandomCharacter()
+                    };
+                    button.Click += Button_Click;
+                    tableLayoutPanel1.Controls.Add(button, j, i);
+                }
+            }
+        }
+
+        private void InitializeTimer()
+        {
+            checkForMatchTimer = new Timer
+            {
+                Interval = 1000,
+                Enabled = false
+            };
+            checkForMatchTimer.Tick += CheckForMatchTimer_Tick;
+        }
+
+        private void CheckForMatchTimer_Tick(object sender, EventArgs e)
+        {
+            HideIfNotMatch();
+            checkForMatchTimer.Stop();
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = (Button)sender;
+            if (clickedButton == firstSelectedButton || clickedButton == secondSelectedButton)
+            {
+                return;
+            }
+            clickedButton.Text = clickedButton.Tag.ToString();
+            if (firstSelectedButton == null)
+            {
+                firstSelectedButton = clickedButton;
+                hideFirstSelectionTimer.Start();
+                return;
+            }
+            secondSelectedButton = clickedButton;
+            hideFirstSelectionTimer.Stop();
+            if (!IsMatch())
+            {
+                checkForMatchTimer.Start();
+            }
+            else
+            {
+                ResetSelection();
+            }
+        }
+
+        private bool IsMatch()
+        {
+            return firstSelectedButton.Tag.ToString() == secondSelectedButton.Tag.ToString();
+        }
+
+        private void HideIfNotMatch()
+        {
+            if (!IsMatch())
+            {
+                firstSelectedButton.Text = "";
+                secondSelectedButton.Text = "";
+            }
+            ResetSelection();
+        }
+
+        private void ResetSelection()
+        {
+            firstSelectedButton = null;
+            secondSelectedButton = null;
+        }
+        private char GetRandomCharacter()
+        {
+            int index = random.Next(characters.Count);
+            char c = characters[index];
+            characters.RemoveAt(index);
+            return c;
+        }
+        private char GetRandomChar()
+        {
+            int num = random.Next(0, 26);
+            char letter = (char)('A' + num);
+            return letter;
+        }
+        private void InitializeCharacters()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                char c = GetRandomChar();
+                characters.Add(c);
+                characters.Add(c);
+            }
+        }
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            timeElapsed++;
+            timeLabel.Text = "Segundos: " + timeElapsed;
+        }
+        private void StartGame()
+        {
+            timeElapsed = 0;
+            gameTimer.Start();
+        }
+
+        private void EndGame()
+        {
+            gameTimer.Stop();
+        }
+        private void HideFirstSelectionTimer_Tick(object sender, EventArgs e)
+        {
+            if (firstSelectedButton != null)
+            {
+                firstSelectedButton.Text = "";
+                ResetSelection();
+            }
+            hideFirstSelectionTimer.Stop();
+        }
+    }
+}
